27274369945173233e14c5e92511be1f0170201b
Michael Bernhart <michael.bernhart@oberalp.com>
2022-03-10 12:24:31 +0100

    Initial Commit

---
+++ Zstack/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Zstack
+//
+//  Created by OakHost Customer on 10/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Zstack/ZstackApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZstackApp.swift
+//  Zstack
+//
+//  Created by OakHost Customer on 10/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct ZstackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
